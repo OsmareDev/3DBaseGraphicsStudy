70bede64b08b0c6a8470300f0059b861878ffa8b
unknown <heartlessome>
2022-08-19 23:20:31 +0200

    Ventana convertida en una clase, limpiando asi el fichero main

---
+++ src/Window.cpp
@@ -0,0 +1,140 @@
+#include "Window.h"
+
+Window Window::main_window;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    LRESULT result = 0;
+
+    switch (uMsg)
+    {
+        case WM_CLOSE:
+        case WM_DESTROY:
+        {
+            Window::GetInstance().window_running = false;
+        } break;
+        case WM_SIZE:
+        {
+            RECT rect;
+            GetClientRect(Window::GetInstance().window, &rect);
+            Window::GetInstance().window_width = rect.right - rect.left;
+            Window::GetInstance().window_height = rect.bottom - rect.top;
+
+            
+        }
+        default:
+        {
+            result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }
+    }
+    return result;
+}
+
+void Window::RecalculateMemory()
+{
+    bufferSize = window_width * window_height * sizeof(unsigned int);
+
+    // si ya habia memoria reservada se libera
+    if (Window::GetInstance().memory) VirtualFree(memory, 0, MEM_RELEASE);
+
+    memory = VirtualAlloc(
+        0,
+        bufferSize,
+        MEM_RESERVE | MEM_COMMIT,
+        PAGE_READWRITE
+    );
+
+    bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
+    bitmap_info.bmiHeader.biWidth = window_width;
+    bitmap_info.bmiHeader.biHeight = window_height;
+    bitmap_info.bmiHeader.biPlanes = 1;
+    bitmap_info.bmiHeader.biBitCount = 32;
+    bitmap_info.bmiHeader.biCompression = BI_RGB;
+}
+
+void Window::StartLoop()
+{
+    RunningLoop();
+}
+
+Window::Window()
+{
+    // de momento parece que sirve
+    hInstance = GetModuleHandle(NULL);
+    nCmdShow = 10;
+    
+    RecalculateMemory();
+
+    DefineClass();
+    WindowCreate();
+}
+
+void Window::DefineClass()
+{
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    if (!RegisterClass(&wc)) { MessageBoxW(nullptr, L"Error registrando la clase", L"Error registrando la clase", MB_OK); }
+}
+
+void Window::WindowCreate()
+{
+    window = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        CLASS_NAME,                     // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // pos.x, pos.y, width, height
+        0, 0, window_width, window_height,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (window == NULL) { MessageBoxW(nullptr, L"Error creando ventana", L"Error creando ventana", MB_OK); }
+    else { ShowWindow(window, nCmdShow); hdc = GetDC(window); window_running = true; }
+}
+
+void Window::RunningLoop()
+{
+    // Message Loop
+    MSG msg = { };
+
+    while (window_running)
+    {
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) > 0)
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        // Draw Loop
+        Render();
+
+        StretchDIBits(
+            hdc,
+            0,
+            0,
+            window_width,
+            window_height,
+            0,
+            0,
+            window_width,
+            window_height,
+            memory,
+            &bitmap_info,
+            DIB_RGB_COLORS,
+            SRCCOPY
+        );
+    }
+}
+
+void Window::Render()
+{
+
+}
---
+++ src/Window.h
@@ -0,0 +1,61 @@
+#ifndef __WINDOW__
+#define __WINDOW__
+
+#pragma once
+
+#include <windows.h>
+#include <string>
+
+
+class Window {
+public:
+	Window(const Window&) = delete;
+
+	static Window& GetInstance() {
+		return main_window;
+	}
+
+	// Getters
+	const int& GetWidth() const { return window_width; }
+	const int& GetHeight() const { return window_height; }
+	const std::wstring& GetTitle() const { return window_title; }
+
+	// Setters
+	void SetWidth(const int& width) { window_width = width; }
+	void SetHeight(const int& height) { window_height = height; }
+	void SetResolution(const int& w, const int& h) { window_width = w, window_height = h; }
+	void SetTitle(const std::wstring& title) { window_title = title; }
+
+	// Friends
+	friend LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	// Functions
+	void Render();
+	void StartLoop();
+	
+private:
+	Window();
+	void DefineClass();
+	void WindowCreate();
+	void RunningLoop();
+	void RecalculateMemory();
+
+	static Window main_window;
+
+	int window_width = 1280, window_height = 720;
+	std::wstring window_title = L"Void Engine";
+	bool window_running = false;
+	int bufferSize;
+	void* memory;
+
+	// WIN32
+	const wchar_t* CLASS_NAME = L"VoidEngine Window";
+	WNDCLASS wc = { };
+	HWND window;
+	HINSTANCE hInstance;
+	int nCmdShow; 
+	HDC hdc;
+	BITMAPINFO bitmap_info;
+};
+
+#endif //__WINDOW__
